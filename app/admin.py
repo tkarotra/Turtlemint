83ebbf9a871a55c03241fadfa76734861a1ddfd5
teesha-13 <94960535+teesha-13@users.noreply.github.com>
2022-12-10 11:43:10 +0530

    Tables Made

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
+
+class ModelField(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ['make', 'model', 'year', 'engine_fuel_type', 
+    'engine_hp', 'engine_cylinders', 'transmission_type', 
+    'driven_wheels', 'no_of_doors', 'market_category', 
+    'vehicle_size', 'vehicle_style', 'highway_mpg', 'city_mpg', 
+    'msrp']
+
+class CompanyField(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(Company, CompanyField)
+admin.site.register(Model, ModelField)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=50, default="")
+
+class Model(models.Model):
+    make = models.ForeignKey(Company, on_delete=models.SET_DEFAULT, default="1")
+    model = models.CharField(max_length=50, default="")
+    year = models.IntegerField(default="")
+    engine_fuel_type = models.CharField(max_length=100, default="")
+    engine_hp = models.IntegerField(default="")
+    engine_cylinders = models.IntegerField(default="")
+    transmission_type = models.CharField(max_length=30, default="")
+    driven_wheels = models.CharField(max_length=30, default="")
+    no_of_doors = models.IntegerField(default="")
+    market_category = models.CharField(max_length=100, default="")
+    vehicle_size = models.CharField(max_length=20, default="")
+    vehicle_style = models.CharField(max_length=30, default="")
+    highway_mpg = models.IntegerField(default="")
+    city_mpg = models.IntegerField(default="")
+    msrp = models.IntegerField(default="")
